198ae8df63ab5e0558808da7327848c4ca4a92ec
zknb-666 <2865467965@qq.com>
2026-02-21 13:33:20 +0800

    Hello Avium

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -20,6 +20,44 @@ PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
+# AVIUM
+# AVIUM_VERSION_APPEND_TIME_OF_DAY is a boolean flag to indicate
+# whether to append time of day to the build date.
+AVIUM_VERSION_APPEND_TIME_OF_DAY ?= true
+# Maintainer
+# AVIUM_MAINTAINER is a string that represents the maintainer of the build.
+AVIUM_MAINTAINER ?= KevinZ
+
+# Settings
+# Soc model name
+AVIUM_SETTINGS_SOC_MODEL_NAME ?= Snapdragon 730G
+# Device code name
+AVIUM_SETTINGS_DEVICE_CODENAME ?= phoenix
+
+# GMS
+# WITH_GMS is a boolean flag to indicate 
+# whether to include Google Mobile Services (GMS) in the build.
+WITH_GMS ?= false
+# Google Sans
+# Enable this to set default fonts to Google Sans.
+# This also works when building vanilla, but you
+# need to sync vendor/pixel/gsans repo.
+TARGET_USES_GSANS := false
+# LatinIMEGooglePrebuilt
+# Only works on vanilla builds,
+# GMS builds will use the Google IME from GMS.
+TARGET_INCLUDE_GOOGLEIME ?= false
+TARGET_GOOGLEIME_OVERRIDE_IME ?= false
+
+# Spoof Props
+# Set to true to enable spoofing fake props.
+# For letting apps think they are running on a locked device.
+AVIUM_FORCE_SET_FAKE_PROP ?= true
+
+# Blur Effect
+# The blur usually enabled on Android 16 QPR2.
+# If the blur not enabled, set to true to force enable blur for SystemUI.
+TARGET_FORCE_ENABLE_BLUR ?= true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
